54da271a7f7a70b64b6015798ccc89f4aa654eb1
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-06-03 13:24:14 +0200

    optimization numpy way

--- mandelbort.py
+++ mandelbort.py
@@ -33,31 +33,48 @@ coord_set['heigth'] = coord_set['y2'] - coord_set['y1']
 
 display = Display(WIDTH, HEIGTH)
 
-@jit
-def mandelbrot_cal(z):
-	# is an array
-	c = z
-	for n in range(max_iter):
-		sqrZ = z * z
-		if sqrZ > 4:
-			return n
-		z = sqrZ + c
-	return 0
+# @jit
+# def mandelbrot_cal(z):
+# 	# is an array
+# 	c = z
+# 	for n in range(max_iter):
+# 		sqrZ = z * z
+# 		if sqrZ > 4:
+# 			return n
+# 		z = sqrZ + c
+# 	return 0
 
-@jit
+# @jit
+# def mandelbrot(xMin, xMax, yMin, yMax):
+# 	x_line = numpy.linspace(xMin, xMax, WIDTH, dtype=numpy.float64)
+# 	y_line = numpy.linspace(yMin, yMax, HEIGTH, dtype=numpy.float64)
+# 	pxa = numpy.empty((WIDTH, HEIGTH))
+# 	for i in range(WIDTH):
+# 		for j in range(HEIGTH):
+# 			pxa[i, j] = mandelbrot_cal(x_line[i] + 1j * y_line[j])
+# 	return x_line, y_line, pxa
+
+def mandelbrot_cal(c):
+	output = numpy.zeros(c.shape)
+	z = numpy.zeros(c.shape, numpy.complex64)
+	for n in range(max_iter):
+		small = numpy.less(z.real * z.real + z.imag * z.imag, 4.0)
+		output[small] = n
+		z[small] = z[small] * z[small] + c[small]
+	output[output == max_iter - 1] = 0
+	return output
+
 def mandelbrot(xMin, xMax, yMin, yMax):
 	x_line = numpy.linspace(xMin, xMax, WIDTH, dtype=numpy.float64)
 	y_line = numpy.linspace(yMin, yMax, HEIGTH, dtype=numpy.float64)
-	pxa = numpy.empty((WIDTH, HEIGTH))
-	for i in range(WIDTH):
-		for j in range(HEIGTH):
-			pxa[i, j] = mandelbrot_cal(x_line[i] + 1j * y_line[j])
-	return x_line, y_line, pxa
+	c = x_line + 1j * y_line[:, None]
+	pxa = mandelbrot_cal(c)
+	return x_line, y_line, pxa.T
 
 def draw():
 
 	start = process_time()
-	_, _, arMap = mandelbrot(coord_set['x1'], coord_set['x2'], coord_set['y1'], coord_set['y2'])
+	_, _, arMap = mandelbrot(coord_set['x1'] / zoom, coord_set['x2'] / zoom, coord_set['y1'] / zoom, coord_set['y2'] / zoom)
 	end = process_time()
 	print('done in ', end - start)
 	display.setSurface(pygame.surfarray.make_surface(arMap))
@@ -102,20 +119,12 @@ def handleMouse(button):
 		return
 
 	if button == 1:
-		zoom *= 1.1
+		zoom *= 1.25
 	elif button == 3:
-		zoom /= 1.1
+		zoom /= 1.25
 
 	print('zoom', zoom)
-	coord_set['x1'] /= zoom
-	coord_set['x2'] /= zoom
-	coord_set['y1'] /= zoom
-	coord_set['y2'] /= zoom
 
-	coord_set['width'] = coord_set['x2'] - coord_set['x1']
-	coord_set['heigth'] = coord_set['y2'] - coord_set['y1']
-
-	print('coord', coord_set)
 	draw()
 
 def eventHandler(type_event, event):
